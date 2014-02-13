0f082b13030b66249db328ebe177fbc200e9711a
Jonathan Raiman <jonathanraiman@gmail.com>
2014-02-13 18:59:51 -0500

    deep belief net outline

--- lib/deep_belief_network/DBN.rb
+++ lib/deep_belief_network/DBN.rb
@@ -1,21 +1,45 @@
 class Optimizer
 	module RBM
 		class HiddenLayer
-			# may not work as of yet.
+			# TODO: implement this in C++ perhaps?
 			attr_accessor :parameters
+			attr_reader :number_of_inputs
+			attr_reader :number_of_outputs
 
-			def initialize inputs, outputs
-				@inputs = inputs
-				@outputs = outputs
+			def initialize n_inputs, n_outputs
+				@number_of_inputs = inputs
+				@number_of_outputs = outputs
 				@parameters = Shark::RealVector.new(0)
 			end
 
+			def sigmoid x
+				1.0 / (1.0 + Math.exp(x))
+			end
+
+			def output inputlayer, weights, bias
+				sigmoid(inputlayer*weights + bias)
+			end
+
+			def biasvalue index
+				## some special Shark operation
+				0.0
+			end
+
 			def sample_h_given_v inputlayer
+				binomial = Shark::Binomial.new
+				solution = []
+				@number_of_outputs.times do |i|
+					solution = binomial.sample 1, output(input, @parameters[i], @parameters[biasvalue(i)])
+				end
+
+				# TODO: create this function according to https://github.com/yusugomori/DeepLearning/blob/master/cpp/DBN.cpp
 				Shark::UnlabeledData.new([])
 			end
 		end
 
 		class DBN
+			# Deep Belief Network.
+
 			def create_rbms_from_layers hidden_layer_array
 
 				hidden_layer_array.each_with_index do |layer_size, k|
@@ -65,6 +89,14 @@ class Optimizer
 						end
 					end
 				end
+			end
+
+			def predict
+				# TODO: write this (Feb 13th 2014)
+			end
+
+			def finetune
+				# requires supervised labels and log layer for logistic regression (Feb 13th 2014)
 			end
 		end
 	end
