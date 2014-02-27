8f7ee2b39647f208aabea88e2d6949b358005c0c
Jonathan Raiman <jonathanraiman@gmail.com>
2014-02-27 16:41:36 -0500

    modified json + header files

--- ext/rb_RNG_Dirichlet.cpp
+++ ext/rb_RNG_Dirichlet.cpp
@@ -13,7 +13,7 @@ VALUE rb_RNG_Dirichlet::rb_class() {
 	return rb_optimizer_rng_dirichlet_klass;
 }
 
-VALUE method_rb_RNG_Dirichlet_set_alphas (VALUE self, VALUE parameter_1, VALUE parameter_2) {
+VALUE method_rb_RNG_Dirichlet_set_alphass (VALUE self, VALUE parameter_1, VALUE parameter_2) {
 	rb_RNG_Dirichlet * r;
 	Data_Get_Struct(self, rb_RNG_Dirichlet, r);
 	
@@ -24,7 +24,7 @@ VALUE method_rb_RNG_Dirichlet_set_alphas (VALUE self, VALUE parameter_1, VALUE p
 	// Checking whether parameter_2 is a "double"
 	if (TYPE(parameter_2) != T_FIXNUM && TYPE(parameter_2) != T_FLOAT)
 		rb_raise(rb_eArgError, "Argument 2 must be a Float.");
-	r->getModel()->alphas(NUM2INT(parameter_1), NUM2DBL(parameter_2));
+	r->getModel()->alphass(NUM2INT(parameter_1), NUM2DBL(parameter_2));
 	return self;
 }
 
@@ -44,7 +44,7 @@ VALUE method_rb_RNG_Dirichlet_initialize (VALUE self) {
 }
 
 void Init_rb_RNG_Dirichlet () {
-	rb_define_method(rb_RNG_Dirichlet::rb_class(), "alphas=", (rb_method) method_rb_RNG_Dirichlet_set_alphas, 2);
+	rb_define_method(rb_RNG_Dirichlet::rb_class(), "alphass=", (rb_method) method_rb_RNG_Dirichlet_set_alphass, 2);
 	rb_define_method(rb_RNG_Dirichlet::rb_class(), "alphas", (rb_method) method_rb_RNG_Dirichlet_get_alphas, 0);
 	rb_define_alloc_func(rb_RNG_Dirichlet::rb_class(), (rb_alloc_func_t) method_rb_RNG_Dirichlet_allocate);
 	rb_define_method(rb_RNG_Dirichlet::rb_class(), "initialize", (rb_method) method_rb_RNG_Dirichlet_initialize, 0);
