44d2127d1f0baed28406dc841cf7529a33838e97
Jonathan Raiman <jonathanraiman@gmail.com>
2014-02-27 18:41:24 -0500

    modified json + header files

--- ext/rb_RNG_Dirichlet.cpp
+++ ext/rb_RNG_Dirichlet.cpp
@@ -13,42 +13,38 @@ VALUE rb_RNG_Dirichlet::rb_class() {
 	return rb_optimizer_rng_dirichlet_klass;
 }
 
-VALUE method_rb_RNG_Dirichlet_set_alphas (VALUE self, VALUE parameter_1, VALUE parameter_2) {
+VALUE method_rb_RNG_Dirichlet_set_alphas (VALUE self, VALUE parameter_1) {
 	rb_RNG_Dirichlet * r;
 	Data_Get_Struct(self, rb_RNG_Dirichlet, r);
 	
-	// Checking whether parameter_1 is an "integer"
-	if (TYPE(parameter_1) != T_FIXNUM && TYPE(parameter_1) != T_FLOAT)
-		rb_raise(rb_eArgError, "Argument 1 must be an Integer.");
+	// Checking whether parameter_1 is a "std::vector<double>"
+	if (TYPE(parameter_1) != T_ARRAY || (RARRAY_LEN(parameter_1) > 0 && TYPE(rb_ary_entry(parameter_1, 0)) != T_FLOAT && TYPE(rb_ary_entry(parameter_1, 0)) != T_FIXNUM) && CLASS_OF(parameter_1) != rb_RealVector::rb_class() && CLASS_OF(parameter_1) != rb_RealVectorReference::rb_class() && CLASS_OF(parameter_1) != rb_RealMatrixColumn::rb_class() && CLASS_OF(parameter_1) != rb_RealMatrixRow::rb_class())
+		rb_raise(rb_eArgError, "Argument 1 must be an ArrayType (\"RealVector\", \"RealVector\", \"RealMatrixColumn\", \"RealMatrixRow\", \"Array\").");
 
-	// Checking whether parameter_2 is a "std::vector<double>"
-	if (TYPE(parameter_2) != T_ARRAY || (RARRAY_LEN(parameter_2) > 0 && TYPE(rb_ary_entry(parameter_2, 0)) != T_FLOAT && TYPE(rb_ary_entry(parameter_2, 0)) != T_FIXNUM) && CLASS_OF(parameter_2) != rb_RealVector::rb_class() && CLASS_OF(parameter_2) != rb_RealVectorReference::rb_class() && CLASS_OF(parameter_2) != rb_RealMatrixColumn::rb_class() && CLASS_OF(parameter_2) != rb_RealMatrixRow::rb_class())
-		rb_raise(rb_eArgError, "Argument 2 must be an ArrayType (\"RealVector\", \"RealVector\", \"RealMatrixColumn\", \"RealMatrixRow\", \"Array\").");
-
-	// Converting parameters "parameter_2" before they can be used.
-	if (TYPE(parameter_2) == T_ARRAY) {
-		RealVector parameter_2_converted = rb_ary_to_1d_realvector(parameter_2);
-		r->getModel()->alphas(NUM2INT(parameter_1), realvector_to_stdvectordouble(parameter_2_converted));
+	// Converting parameters "parameter_1" before they can be used.
+	if (TYPE(parameter_1) == T_ARRAY) {
+		RealVector parameter_1_converted = rb_ary_to_1d_realvector(parameter_1);
+		r->getModel()->alphas(realvector_to_stdvectordouble(parameter_1_converted));
 		return self;
-	} else if (CLASS_OF(parameter_2) == rb_RealVector::rb_class()) {
-		rb_RealVector * parameter_2_converted;
-		Data_Get_Struct(parameter_2, rb_RealVector, parameter_2_converted);
-		r->getModel()->alphas(NUM2INT(parameter_1), realvector_to_stdvectordouble(*(parameter_2_converted->getData())));
+	} else if (CLASS_OF(parameter_1) == rb_RealVector::rb_class()) {
+		rb_RealVector * parameter_1_converted;
+		Data_Get_Struct(parameter_1, rb_RealVector, parameter_1_converted);
+		r->getModel()->alphas(realvector_to_stdvectordouble(*(parameter_1_converted->getData())));
 		return self;
-	} else if (CLASS_OF(parameter_2) == rb_RealVectorReference::rb_class()) {
-		rb_RealVectorReference * parameter_2_converted;
-		Data_Get_Struct(parameter_2, rb_RealVectorReference, parameter_2_converted);
-		r->getModel()->alphas(NUM2INT(parameter_1), realvector_to_stdvectordouble(*(parameter_2_converted->getData())));
+	} else if (CLASS_OF(parameter_1) == rb_RealVectorReference::rb_class()) {
+		rb_RealVectorReference * parameter_1_converted;
+		Data_Get_Struct(parameter_1, rb_RealVectorReference, parameter_1_converted);
+		r->getModel()->alphas(realvector_to_stdvectordouble(*(parameter_1_converted->getData())));
 		return self;
-	} else if (CLASS_OF(parameter_2) == rb_RealMatrixColumn::rb_class()) {
-		rb_RealMatrixColumn * parameter_2_converted;
-		Data_Get_Struct(parameter_2, rb_RealMatrixColumn, parameter_2_converted);
-		r->getModel()->alphas(NUM2INT(parameter_1), realvector_to_stdvectordouble(*(parameter_2_converted->getData())));
+	} else if (CLASS_OF(parameter_1) == rb_RealMatrixColumn::rb_class()) {
+		rb_RealMatrixColumn * parameter_1_converted;
+		Data_Get_Struct(parameter_1, rb_RealMatrixColumn, parameter_1_converted);
+		r->getModel()->alphas(realvector_to_stdvectordouble(*(parameter_1_converted->getData())));
 		return self;
-	} else if (CLASS_OF(parameter_2) == rb_RealMatrixRow::rb_class()) {
-		rb_RealMatrixRow * parameter_2_converted;
-		Data_Get_Struct(parameter_2, rb_RealMatrixRow, parameter_2_converted);
-		r->getModel()->alphas(NUM2INT(parameter_1), realvector_to_stdvectordouble(*(parameter_2_converted->getData())));
+	} else if (CLASS_OF(parameter_1) == rb_RealMatrixRow::rb_class()) {
+		rb_RealMatrixRow * parameter_1_converted;
+		Data_Get_Struct(parameter_1, rb_RealMatrixRow, parameter_1_converted);
+		r->getModel()->alphas(realvector_to_stdvectordouble(*(parameter_1_converted->getData())));
 		return self;
 	}
 
@@ -71,7 +67,7 @@ VALUE method_rb_RNG_Dirichlet_initialize (VALUE self) {
 }
 
 void Init_rb_RNG_Dirichlet () {
-	rb_define_method(rb_RNG_Dirichlet::rb_class(), "alphas=", (rb_method) method_rb_RNG_Dirichlet_set_alphas, 2);
+	rb_define_method(rb_RNG_Dirichlet::rb_class(), "alphas=", (rb_method) method_rb_RNG_Dirichlet_set_alphas, 1);
 	rb_define_method(rb_RNG_Dirichlet::rb_class(), "alphas", (rb_method) method_rb_RNG_Dirichlet_get_alphas, 0);
 	rb_define_alloc_func(rb_RNG_Dirichlet::rb_class(), (rb_alloc_func_t) method_rb_RNG_Dirichlet_allocate);
 	rb_define_method(rb_RNG_Dirichlet::rb_class(), "initialize", (rb_method) method_rb_RNG_Dirichlet_initialize, 0);
