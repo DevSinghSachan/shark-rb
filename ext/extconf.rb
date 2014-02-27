24257d667e7d812c0941709dd8c463c21b04b5eb
Jonathan Raiman <jonathanraiman@gmail.com>
2014-02-27 15:56:26 -0500

    modified json + header files

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,7 +1,7 @@
 # Loads mkmf which is used to make makefiles for Ruby extensions
 require 'mkmf'
-# extension_name = 'rb_shark'
-# dir_config(extension_name)
+extension_name = 'rb_shark'
+dir_config(extension_name)
 
 have_header("ruby.h")
 have_library("shark")
@@ -12,6 +12,4 @@ have_library("boost_system-mt")
 have_library("boost_serialization-mt")
 $libs = append_library($libs, "stdc++")
 
-if __FILE__ == $0
-	create_makefile(extension_name)
-end
+create_makefile(extension_name)
