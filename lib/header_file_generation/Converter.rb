243fa40717cbedb691edc7394d2b3cdb431bb53c
Jonathan Raiman <jonathanraiman@gmail.com>
2014-02-28 16:22:51 -0500

    modified json + header files

--- lib/header_file_generation/Converter.rb
+++ lib/header_file_generation/Converter.rb
@@ -4,40 +4,52 @@ module HeaderFileGenerator
 
 			def self.create_type_conversion conversion_function, type
 				cpp_class = Method::CppClass.new(type)
-				->(input_param_name, out_param_name=nil, indent=0) {
+				->(input_param_name, out_param_name=nil, indent=0, pointer=false) {
 					if out_param_name
 						"#{"\t"*indent}#{cpp_class.cpp_class} #{out_param_name} = #{conversion_function}(#{input_param_name})"
 					else
-						"#{conversion_function}(#{input_param_name})"
+						if pointer
+							"&(#{conversion_function}(#{input_param_name}))"
+						else
+							"#{conversion_function}(#{input_param_name})"
+						end
 					end
 				}
 			end
 
 			def self.create_cast_conversion type
 				cpp_class = Method::CppClass.new(type)
-				->(input_param_name, out_param_name=nil, indent=0) {
+				->(input_param_name, out_param_name=nil, indent=0, pointer=false) {
 					if out_param_name
 						"#{"\t"*indent}#{cpp_class.cpp_class} #{out_param_name} = #{input_param_name}"
 					else
-						"#{input_param_name}"
+						if pointer
+							"&(#{input_param_name})"
+						else
+							"#{input_param_name}"
+						end
 					end
 				}
 			end
 
 			def self.create_wrapped_class_conversion conversion_function, type
 				cpp_class = Method::CppClass.new(type)
-				->(input_param_name, out_param_name=nil, indent=0) {
+				->(input_param_name, out_param_name=nil, indent=0, pointer=false) {
 					if out_param_name
 						"#{"\t"*indent}#{cpp_class.pointer} #{out_param_name} = #{input_param_name}->#{conversion_function}()"
 					else
-						"#{input_param_name}->#{conversion_function}()"
+						if pointer
+							"&(#{input_param_name}->#{conversion_function}())"
+						else
+							"#{input_param_name}->#{conversion_function}()"
+						end
 					end
 				}
 			end
 
 			def self.create_ruby_conversion pointer_name
 				rb_cpp_class = Method::CppClass.new(pointer_name)
-				->(input_param_name, out_param_name, indent=0) {
+				->(input_param_name, out_param_name, indent=0, pointer=false) {
 """
 #{"\t"*indent}#{rb_cpp_class.pointer} #{out_param_name};
 #{"\t"*indent}Data_Get_Struct(#{input_param_name}, #{rb_cpp_class}, #{out_param_name});
@@ -46,7 +58,7 @@ module HeaderFileGenerator
 			end
 
 			def self.create_combined_conversion conversion_a, conversion_b
-				->(input_param_name, out_param_name, indent=0) {
+				->(input_param_name, out_param_name, indent=0, pointer=false) {
 					(
 						conversion_a.call(input_param_name, out_param_name+"_temp", indent) +
 						conversion_b.call(out_param_name+"_temp", out_param_name, indent)
@@ -165,7 +177,7 @@ module HeaderFileGenerator
 
 			def from(type)
 				if !@converter then raise ArgumentError.new "Can't since it already has an origin type defined, #{@origin.cpp_class}." end
-				@origin = type.to_s
+				@origin = type
 				to_s_if_ready!
 			end
 
@@ -173,45 +185,29 @@ module HeaderFileGenerator
 				if @origin.nil? || @destination.nil? then raise ArgumentError.new "Converter needs an origin and destination type: call #from and #to." end
 				if @origin == @destination then @name
 				else
-					Conversions.fetch(@origin).fetch(@destination).call(@name)
+					Conversions.fetch(@origin.to_s).fetch(@destination.to_s).call(@name,nil,0,(@destination.is_a? Method::CppClass) ? @destination.pointer? : nil)
 				end
 			rescue KeyError
 				raise NotImplementedError.new "No conversion from #{@origin} to #{@destination}."
 			end
 
-			def remove_pointer?
-				@origin.to_s == @destination.to_s + "*"
-			end
-
-			def add_pointer?
-				@destination.to_s == @origin.to_s + "*"
-			end
-
 			def to(type)
 				if !@converter
 					@destination = type
-					if remove_pointer?
-						puts "yes"
-						"*"
-					elsif add_pointer?
-						puts "ok"
-						"&"
-					else
-						# puts "@origin.to_s => #{@origin.to_s}"
-						origin_type = Conversions[@origin.to_s]
-						if origin_type
-							# puts "@destination.to_s => #{@destination.to_s}"
-							if origin_type[@destination.to_s]
-								true
-							else
-								false
-							end
+					# puts "@origin.to_s => #{@origin.to_s}"
+					origin_type = Conversions[@origin.to_s]
+					if origin_type
+						# puts "@destination.to_s => #{@destination.to_s}"
+						if origin_type[@destination.to_s]
+							true
 						else
 							false
 						end
+					else
+						false
 					end
 				else
-					@destination = type.to_s
+					@destination = type
 					to_s_if_ready!
 				end
 			end
