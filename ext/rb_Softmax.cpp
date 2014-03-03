c75dd68093e4a8a48d53a1fd2c90d803f576ec6b
Jonathan Raiman <jonathanraiman@gmail.com>
2014-03-03 08:46:35 -0500

    modified json + header files

--- ext/rb_Softmax.cpp
+++ ext/rb_Softmax.cpp
@@ -14,17 +14,6 @@ VALUE rb_Softmax::rb_class() {
 	return rb_optimizer_rng_poisson_klass;
 }
 
-VALUE method_rb_Softmax_set_set_structure (VALUE self, VALUE parameter_1) {
-	rb_Softmax * r;
-	Data_Get_Struct(self, rb_Softmax, r);
-	
-	// Checking whether parameter_1 is an "int"
-	if (TYPE(parameter_1) != T_FIXNUM && TYPE(parameter_1) != T_FLOAT)
-		rb_raise(rb_eArgError, "Argument 1 must be an Integer.");
-	r->getModel()->setStructure(NUM2INT(parameter_1));
-	return self;
-}
-
 VALUE method_rb_Softmax_get_number_of_outputs (VALUE self) {
 	rb_Softmax * r;
 	Data_Get_Struct(self, rb_Softmax, r);
@@ -47,6 +36,17 @@ VALUE method_rb_Softmax_initialize (VALUE self) {
 	return self;
 }
 
+VALUE method_rb_Softmax_set_structure (VALUE self, VALUE parameter_1) {
+	rb_Softmax * r;
+	Data_Get_Struct(self, rb_Softmax, r);
+	
+	// Checking whether parameter_1 is an "int"
+	if (TYPE(parameter_1) != T_FIXNUM && TYPE(parameter_1) != T_FLOAT)
+		rb_raise(rb_eArgError, "Argument 1 must be an Integer.");
+	r->getModel()->setStructure(NUM2INT(parameter_1));
+	return self;
+}
+
 VALUE method_rb_Softmax_eval (VALUE self, VALUE parameter_1) {
 	rb_Softmax * r;
 	Data_Get_Struct(self, rb_Softmax, r);
@@ -60,39 +60,39 @@ VALUE method_rb_Softmax_eval (VALUE self, VALUE parameter_1) {
 
 		rb_RealVector * parameter_1_converted;
 		Data_Get_Struct(parameter_1, rb_RealVector, parameter_1_converted);
-		return wrap_pointer<rb_UnlabeledData>(rb_UnlabeledData::rb_class(), new rb_UnlabeledData(r->getModel()->eval(*(parameter_1_converted->getData()))));
+		return wrap_pointer<rb_RealVector>(rb_RealVector::rb_class(), new rb_RealVector((*(r->getModel()))(*(parameter_1_converted->getData()))));
 	} else if (CLASS_OF(parameter_1) == rb_RealVectorReference::rb_class()) {
 
 		rb_RealVectorReference * parameter_1_converted;
 		Data_Get_Struct(parameter_1, rb_RealVectorReference, parameter_1_converted);
-		return wrap_pointer<rb_UnlabeledData>(rb_UnlabeledData::rb_class(), new rb_UnlabeledData(r->getModel()->eval(*(parameter_1_converted->getData()))));
+		return wrap_pointer<rb_RealVector>(rb_RealVector::rb_class(), new rb_RealVector((*(r->getModel()))(*(parameter_1_converted->getData()))));
 	} else if (CLASS_OF(parameter_1) == rb_RealMatrixColumn::rb_class()) {
 
 		rb_RealMatrixColumn * parameter_1_converted;
 		Data_Get_Struct(parameter_1, rb_RealMatrixColumn, parameter_1_converted);
-		return wrap_pointer<rb_UnlabeledData>(rb_UnlabeledData::rb_class(), new rb_UnlabeledData(r->getModel()->eval(*(parameter_1_converted->getData()))));
+		return wrap_pointer<rb_RealVector>(rb_RealVector::rb_class(), new rb_RealVector((*(r->getModel()))(*(parameter_1_converted->getData()))));
 	} else if (CLASS_OF(parameter_1) == rb_RealMatrixRow::rb_class()) {
 
 		rb_RealMatrixRow * parameter_1_converted;
 		Data_Get_Struct(parameter_1, rb_RealMatrixRow, parameter_1_converted);
-		return wrap_pointer<rb_UnlabeledData>(rb_UnlabeledData::rb_class(), new rb_UnlabeledData(r->getModel()->eval(*(parameter_1_converted->getData()))));
+		return wrap_pointer<rb_RealVector>(rb_RealVector::rb_class(), new rb_RealVector((*(r->getModel()))(*(parameter_1_converted->getData()))));
 	} else if (TYPE(parameter_1) == T_ARRAY) {
 		RealVector parameter_1_converted = rb_ary_to_1d_realvector(parameter_1);
-		return wrap_pointer<rb_UnlabeledData>(rb_UnlabeledData::rb_class(), new rb_UnlabeledData(r->getModel()->eval(parameter_1_converted)));
+		return wrap_pointer<rb_RealVector>(rb_RealVector::rb_class(), new rb_RealVector((*(r->getModel()))(parameter_1_converted)));
 	} else if (TYPE(parameter_1) == T_ARRAY) {
 		RealMatrix parameter_1_converted = rb_ary_to_realmatrix(parameter_1);
-		return wrap_pointer<rb_UnlabeledData>(rb_UnlabeledData::rb_class(), new rb_UnlabeledData(r->getModel()->eval(parameter_1_converted)));
+		return wrap_pointer<rb_RealVector>(rb_RealVector::rb_class(), new rb_RealVector((*(r->getModel()))(parameter_1_converted)));
 	}
 
 	return self; // cpp functions require return variable, so if all tests fail "self" is returned.
 }
 
 void Init_rb_Softmax () {
-	rb_define_method(rb_Softmax::rb_class(), "set_structure=", (rb_method) method_rb_Softmax_set_set_structure, 1);
 	rb_define_method(rb_Softmax::rb_class(), "number_of_outputs", (rb_method) method_rb_Softmax_get_number_of_outputs, 0);
 	rb_define_method(rb_Softmax::rb_class(), "number_of_inputs", (rb_method) method_rb_Softmax_get_number_of_inputs, 0);
 	rb_define_alloc_func(rb_Softmax::rb_class(), (rb_alloc_func_t) method_rb_Softmax_allocate);
 	rb_define_method(rb_Softmax::rb_class(), "initialize", (rb_method) method_rb_Softmax_initialize, 0);
+	rb_define_method(rb_Softmax::rb_class(), "set_structure", (rb_method) method_rb_Softmax_set_structure, 1);
 	rb_define_method(rb_Softmax::rb_class(), "eval", (rb_method) method_rb_Softmax_eval, 1);
 InitAbstractModel<rb_Softmax>();
 }
